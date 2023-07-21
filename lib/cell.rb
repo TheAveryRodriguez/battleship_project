6d7dd2b291551fccdd7d279948e9a3069e657947
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-21 09:53:59 -0500

    create a render method to be tested. Cannot test till completion of fired_upon method.

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,5 +1,7 @@
 class Cell
-  attr_reader :coordinate, :ship
+  attr_reader :coordinate, 
+              :ship
+
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
@@ -11,5 +13,18 @@ class Cell
 
   def place_ship(ship)
     @ship = ship
+  end
+
+  def render(view_ship = false)
+    if !@fired_upon && !empty? && view_ship
+      'S'
+    elsif @fired_upon && empty? 
+      'M'
+    elsif @fired_upon && empty? == false && @ship.sunk?
+      'X'
+    elsif @fired_upon && empty? == false
+      'H'
+    elsif @fired_upon == false 
+      '.'
   end
 end
