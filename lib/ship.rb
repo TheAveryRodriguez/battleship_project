882a49698e13f577350e87381ce7f313d4ca1f51
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-07-20 21:41:20 -0500

    create the ship class

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,1 +1,19 @@
-# Need to create a ship class
+class Ship
+  attr_reader :name, :length, :health
+  def initialize(name, length)
+    @name = name
+    @length = length
+  end
+end
+
+def health
+  health = @length
+end
+
+def hit
+  health - 1
+end
+
+def sunk?
+  health == 0
+end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,1 +1,33 @@
-# Need to create tests for ship class
+require './lib/ship'
+
+cruiser = Ship.new("Cruiser", 3)
+
+expect(cruiser.name
+#=> "Cruiser"
+
+expect(cruiser.length
+#=> 3
+
+expect(cruiser.health
+#=> 3
+
+expect(cruiser.sunk?
+#=> false
+
+cruiser.hit
+
+expect(cruiser.health
+#=> 2
+
+expect(cruiser.hit
+
+expect(cruiser.health
+#=> 1
+
+expect(cruiser.sunk?
+#=> false
+
+expect(cruiser.hit
+
+expect(cruiser.sunk?
+#=> true
