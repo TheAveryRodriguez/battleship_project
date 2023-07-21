f445881e935ed001350f564b617f6f702813649f
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-07-20 23:01:53 -0500

    create the cell class and tests

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,1 +1,15 @@
-# Need to create a cell class
+class Cell
+  attr_reader :coordinate, :ship
+  def initialize(coordinate)
+    @coordinate = coordinate
+    @ship = nil
+  end
+
+  def empty?
+    @ship.nil?
+  end
+
+  def place_ship(ship)
+    @ship = ship
+  end
+end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,1 +1,27 @@
-# Need to create tests for cell class
+require "spec_helper"
+
+RSpec.describe do
+  describe "#initialize" do
+    it "creates a cell instance" do
+      cell = Cell.new("B4")
+
+      expect(cell).to be_a(Cell)
+      expect(cell.coordinate).to eq("B4")
+    end
+  end
+
+  describe "#place_ship" do
+    it "places a ship in a cell" do
+      cell = Cell.new("B4")
+
+      expect(cell.ship).to eq(nil)
+      expect(cell.empty?).to eq(true)
+
+      cruiser = Ship.new("Cruiser", 3)
+
+      cell.place_ship(cruiser)
+      expect(cell.ship).to eq(cruiser)
+      expect(cell.empty?).to eq(false)
+    end
+  end
+end
