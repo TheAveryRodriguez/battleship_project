f09083e2272770506dd87fbcaa00ca73d7d8ea5e
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-24 16:50:13 -0500

    add a test to check valid_placement is passing for a good placement.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -65,6 +65,7 @@ RSpec.describe do
       expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
       expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false) 
       expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false) 
+      expect(board.valid_placement?(submarine, ["B2", "B3"])).to eq(true)
     end
   end
 end
