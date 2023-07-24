49113d3c895bb800c0895817cdba0eebccf8ffb1
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-07-24 17:20:52 -0500

    mistake

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe do
     end
   end
 
-  describe "#cells" do 
-    it "can hold cells on a board" do 
+  describe "#cells" do
+    it "can hold cells on a board" do
       board = Board.new
       expect(board.cells).to be_a(Hash)
 
@@ -32,15 +32,26 @@ RSpec.describe do
     end
   end
 
-  describe "#valid_coordinate?" do 
+  describe "#valid_coordinate?" do
     it "only holds valid coordinates" do
       board = Board.new
-      
+
       expect(board.valid_coordinate?("A1")).to eq(true)
       expect(board.valid_coordinate?("D4")).to eq(true)
       expect(board.valid_coordinate?("A5")).to eq(false)
       expect(board.valid_coordinate?("E1")).to eq(false)
       expect(board.valid_coordinate?("A22")).to eq(false)
     end
-  end 
+  end
+
+  describe "#valid_placemnt?" do
+    it "can place a ship in a coordinate if placement meets perameters" do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+
+      expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
+      expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+    end
+  end
 end
