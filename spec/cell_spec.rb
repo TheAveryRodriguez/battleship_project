eaacdf279fd1ccbd9fce7c41b9b75fe1fb450b5e
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-23 19:50:21 -0500

    add a test for render method logic.

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -4,19 +4,27 @@ RSpec.describe do
   describe "#initialize" do
     it "creates a cell instance" do
       cell = Cell.new("B4")
-
       expect(cell).to be_a(Cell)
+    end
+  end
+
+  describe "#coordinate" do 
+    it 'initializes with a coordinate' do 
+      cell = Cell.new("B4")
       expect(cell.coordinate).to eq("B4")
     end
   end
 
+  describe "#ship" do 
+    it "checks for a ship in coordinate" do 
+      cell = Cell.new("B4")
+      expect(cell.ship).to eq(nil)
+      expect(cell.empty?).to eq(true)
+    end
+  end
   describe "#place_ship" do
     it "places a ship in a cell" do
       cell = Cell.new("B4")
-
-      expect(cell.ship).to eq(nil)
-      expect(cell.empty?).to eq(true)
-
       cruiser = Ship.new("Cruiser", 3)
 
       cell.place_ship(cruiser)
@@ -32,11 +40,20 @@ RSpec.describe do
 
       cell.place_ship(cruiser)
       expect(cell.fired_upon?).to eq(false)
-
       cell.fire_upon
-
       expect(cell.ship.health).to eq(2)
       expect(cell.fired_upon?).to eq(true)
+    end
+  end
+
+  describe "render" do 
+    it "can rendera  cell" do 
+      cell = Cell.new("B4")
+
+      cell = Cell.new("B4")
+      expect(cell.render).to eq('.')
+      cell.fire_upon
+      expect(cell.render).to eq('M')
     end
   end
 end
