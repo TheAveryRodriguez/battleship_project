e1189860b0353b866a8a32428f603fcaad6db62d
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-24 12:33:45 -0500

    spacing correction, line 23.5

--- lib/board.rb
+++ lib/board.rb
@@ -21,23 +21,4 @@ class Board
       "D4" => Cell.new("D4") 
     }
   end
-
-  # def cells
-  #   @cells = {}
-  #   create_cells
-  # end
-
-  # def create_cells
-  #   ("A".."D").each do |letter|
-  #     (1..4).each do |number|
-  #       coordinate = "#{letter}#{number}"
-  #       @cells[coordinate] = Cell.new(coordinate)
-
-  #       # Potential solutions
-  #       # Hash.values.flatten
-  #       # cell_list = Hash.values.flatten
-  #       # expect(cell_list).to all be_a(Cell)
-  #     end
-  #   end
-  # end
 end
