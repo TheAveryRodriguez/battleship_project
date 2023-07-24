cbc3fbaaa8d61b037222dff9d4445e72fbd2aa02
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-24 12:37:24 -0500

    test cells A-D, 1-4 on board class are cell objects.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -14,7 +14,22 @@ RSpec.describe do
       board = Board.new
       expect(board.cells).to be_a(Hash)
 
-      
+      expect(board.cells["A1"]).to be_a(Cell)
+      expect(board.cells["A2"]).to be_a(Cell)
+      expect(board.cells["A3"]).to be_a(Cell)
+      expect(board.cells["A4"]).to be_a(Cell)
+      expect(board.cells["B1"]).to be_a(Cell)
+      expect(board.cells["B2"]).to be_a(Cell)
+      expect(board.cells["B3"]).to be_a(Cell)
+      expect(board.cells["B4"]).to be_a(Cell)
+      expect(board.cells["C1"]).to be_a(Cell)
+      expect(board.cells["C2"]).to be_a(Cell)
+      expect(board.cells["C3"]).to be_a(Cell)
+      expect(board.cells["C4"]).to be_a(Cell)
+      expect(board.cells["D1"]).to be_a(Cell)
+      expect(board.cells["D2"]).to be_a(Cell)
+      expect(board.cells["D3"]).to be_a(Cell)
+      expect(board.cells["D4"]).to be_a(Cell)
     end
   end
 
