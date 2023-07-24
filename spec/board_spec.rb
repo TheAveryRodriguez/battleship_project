9fd74f1d538c2e6e1596f2fb4afd6dbbd57cea20
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-24 13:35:30 -0500

    add testing for valid_coordinate method in board class.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe do
     end
   end
 
-  describe "cells" do 
+  describe "#cells" do 
     it "can hold cells on a board" do 
       board = Board.new
       expect(board.cells).to be_a(Hash)
@@ -31,4 +31,16 @@ RSpec.describe do
       expect(board.cells["D4"]).to be_a(Cell)
     end
   end
+
+  describe "#valid_coordinate?" do 
+    it "only holds valid coordinates" do
+      board = Board.new
+      
+      expect(board.valid_coordinate?("A1")).to eq(true)
+      expect(board.valid_coordinate?("D4")).to eq(true)
+      expect(board.valid_coordinate?("A5")).to eq(false)
+      expect(board.valid_coordinate?("E1")).to eq(false)
+      expect(board.valid_coordinate?("A22")).to eq(false)
+    end
+  end 
 end
