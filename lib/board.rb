46f0911f9d09ab637f92b4706fedcbf371b5880b
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-24 12:33:07 -0500

    remove create cell method in board class.

--- lib/board.rb
+++ lib/board.rb
@@ -27,17 +27,17 @@ class Board
   #   create_cells
   # end
 
-  def create_cells
-    ("A".."D").each do |letter|
-      (1..4).each do |number|
-        coordinate = "#{letter}#{number}"
-        @cells[coordinate] = Cell.new(coordinate)
+  # def create_cells
+  #   ("A".."D").each do |letter|
+  #     (1..4).each do |number|
+  #       coordinate = "#{letter}#{number}"
+  #       @cells[coordinate] = Cell.new(coordinate)
 
-        # Potential solutions
-        # Hash.values.flatten
-        # cell_list = Hash.values.flatten
-        # expect(cell_list).to all be_a(Cell)
-      end
-    end
-  end
+  #       # Potential solutions
+  #       # Hash.values.flatten
+  #       # cell_list = Hash.values.flatten
+  #       # expect(cell_list).to all be_a(Cell)
+  #     end
+  #   end
+  # end
 end
