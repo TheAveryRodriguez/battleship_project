3702e32929232e82f5acf43cf0346ab8872800f8
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-23 19:25:34 -0500

    comment out fire_upon logic, and add render method to class and logic to class.

--- lib/cell.rb
+++ lib/cell.rb
@@ -21,16 +21,33 @@ class Cell
 
   def fire_upon
     @fired_upon = true
-
-    if !@ship.nil?
+    if empty? == false
       @ship.hit
-      if @ship.sunk?
-        "Try Again"
-      else
-        "Oh snap. You hit a ship!"
-      end
-    else
-      "(¯`·._.··¸.-~*´¨¯¨`*·~-.,-(_YOU MISSED SUCKER_)-,.-~*´¨¯¨`*·~-.¸··._.·´¯)"
+    end
+
+    # if !@ship.nil?
+    #   @ship.hit
+    #   if @ship.sunk?
+    #     "Try Again"
+    #   else
+    #     "Oh snap. You hit a ship!"
+    #   end
+    # else
+    #   "(¯`·._.··¸.-~*´¨¯¨`*·~-.,-(_YOU MISSED SUCKER_)-,.-~*´¨¯¨`*·~-.¸··._.·´¯)"
+    # end
+  end
+
+  def render(reveal_ship = false)
+    if !@fired_upon && !empty? && show_ship
+      'S'
+    elsif @fired_upon && empty?
+      'M'
+    elsif @fired_upon && empty? == false && @ship.sunk?
+      'X'
+    elsif @fired_upon && empty? == false
+      'H'
+    elsif @fired_upon == false
+      '.'
     end
   end
 end
