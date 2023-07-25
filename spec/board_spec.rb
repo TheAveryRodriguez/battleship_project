bff41582dd66e0e6075202df330f62a65daa95ec
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-24 21:44:50 -0500

    add testing for not overlapping ship placement.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -117,5 +117,14 @@ RSpec.describe do
 
       expect(board.valid_placement?(submarine, ["A1", "B1"])).to be(false)
     end
+
+    it "cannot overlap ship placements" do 
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+
+      board.place(cruiser, ["A1", "A2", "A3"])
+      expect(board.valid_placement?(submarine, ["A1", "B1"])).to eq(false)
+    end
   end
 end
