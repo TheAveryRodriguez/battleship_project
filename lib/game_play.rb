bcedb7bf59e7a9124016820524020b29c88e3cb1
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-26 12:37:40 -0500

    add turn method to game_play class.

--- lib/game_play.rb
+++ lib/game_play.rb
@@ -8,6 +8,7 @@ attr_reader :computer_board, :computer_cruiser,
     @computer_cruiser = Ship.new("Cruiser", 3)
     @computer_sub = Ship.new("Submarine", 2)
     @computer_sunk_ships = 0 
+
     @player_board = Board.new 
     @player_cruiser = Ship.new("Cruiser", 3)
     @player_sub = Ship.new("Submarine", 2)
@@ -63,23 +64,66 @@ attr_reader :computer_board, :computer_cruiser,
 
   def player_cruiser_placement
     puts "Enter squares for your Cruiser (3 spaces):"
-    @player_cruiser_input = gets.chomp.upcase.split 
-    if @player_board.valid_placement?(@player_cruiser, @player_cruiser_input) == true 
-      @player_board.place(@player_cruiser, @player_cruiser_input)
+    player_cruiser_input = gets.chomp.upcase.split 
+    if @player_board.valid_placement?(@player_cruiser, player_cruiser_input) == true 
+      @player_board.place(@player_cruiser, player_cruiser_input)
     else
       puts "Those are invalid coordinates. Please try again."
-      @player_cruiser_placement 
+      player_cruiser_placement 
     end
   end
 
   def player_sub_placement
     puts "Enter squares for your Submarine (2 spaces):"
-    @player_sub_input = gets.chomp.upcase.split 
-    if @player_board.valid_placement?(@player_sub, @player_sub_input) == true 
-      @player_board.place(@player_sub, @player_sub_input)
+    player_sub_input = gets.chomp.upcase.split 
+    if @player_board.valid_placement?(@player_sub, player_sub_input) == true 
+      @player_board.place(@player_sub, player_sub_input)
     else
       puts "Those are invalid coordinates. Please try again."
-      @player_sub_placement 
+      player_sub_placement 
     end
   end
+
+  def turn 
+    puts "=============COMPUTER BOARD============="
+    puts @computer_board.render
+    puts "==============PLAYER BOARD=============="
+    puts @player_board.render(true)
+    puts "Enter the coordinate for your shot:"
+    player_choice = gets.chomp.upcase
+    if @computer_board.valid_coordinate?(player_choice)
+      @computer_board.cells[player_choice].fire_upon
+      player_result = @computer_board.cells[player_choice].render
+      @computer_sunk_ships += 1 if player_result == "X"
+    else
+      puts "Please enter a valid coordinate."
+      turn
+    end
+    computer_choice = @player_board.cells.keys.sample(1).join
+    until @player_board.cells[computer_choice].fired_upon? == false
+      computer_choice = @player_board.cells.keys.sample(1).join
+    end
+    @player_board.cells[computer_choice].fire_upon
+    computer_result = @player_board.cells[computer_choice].render(true)
+    if computer_result == "X"
+      @player_sunk_ships += 1
+    end
+    puts "Your shot on #{player_choice} was a #{player_result}."
+    puts "My shot on #{computer_choice} was a #{computer_result}."
+    over?
+  end
+
+  def over?
+    if @player_sunk_ships == 2
+      puts "Your Fleet is DESTROYED! Better Luck Next Time!"
+      self.play
+    elsif
+      @computer_sunk_ships == 2
+      puts "You Destroyed My Fleet! YOU WIN!"
+      self.play
+    else
+      turn
+    end
+  end
+  
 end
