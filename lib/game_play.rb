b4331a25d6514bdfe5bdd8658397ecb4b1a5f1dc
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-25 19:51:46 -0500

    add methods to be called on for game play: player cruiser & sub placement, computer sub and cruiser placement, computer message.

--- lib/game_play.rb
+++ lib/game_play.rb
@@ -1,13 +1,24 @@
 class GamePlay
 
+  def game_info
+    computer_board = Board.new 
+    computer_cruiser = Ship.new("Cruiser", 3)
+    computer_sub = Ship.new("Submarine", 2)
+    computer_sunk_ships = 0 
+  
+    player_board = Board.new 
+    player_cruiser = Ship.new("Cruiser", 3)
+    player_sub = Ship.new("Submarine", 2)
+    player_sunk_ships = 0
+  end
+
   def menu 
     puts "Welcome to BATTLESHIP!!
-
     Enter p to play. Enter q to quit."
     input = gets.chomp.downcase
     if input == "p"
       puts "Welcome aboard!"
-      computer_ship_placement
+      computer_sub_placement
     elsif input == "q"
       puts "Shove off, Sailor!"
       menu 
@@ -17,41 +28,42 @@ class GamePlay
     end
   end 
 
-  def game_info
-    computer_board = Board.new 
-    computer_cruiser = Ship.new("Cruiser", 3)
-    computer_sub = Ship.new("Submarine", 2)
-    computer_sunk_ships = 0 
-
-    player_board = Board.new 
-    player_cruiser = Ship.new("Cruiser", 3)
-    player_sub = Ship.new("Submarine", 2)
-    player_sunk_ships = 0
+  def computer_sub_placement 
+    valid_sub_coordinates = computer_board.cells.keys.sample(2)
+    until computer_board.valid_placement?(computer_sub, valid_sub_coordinates) == true 
+      valid_sub_coordinates = computer_board.cells.keys.sample(2)
+    end
+    computer_board.place(computer_sub, valid_sub_coordinates)
+    # player_ship_placement
   end
 
-  def computer_ship_placement 
-    # computer places sub and cruiser in: random coordinates && valid coordinates 
-    # -------- code to be written ---------(.sample) = randomization 
-    puts "I have placed my ships. Now it is your turn."
-    player_ship_placement
+  def computer_cruiser_placement
+    valid_cruiser_coordinates = computer_board.cells.keys.sample(3)
+    until computer_board.valid_placement?(computer_cruiser, valid_cruiser_coordinates) == true 
+      valid_cruiser_coordinates = computer_board.cells.keys.sample(3)
+    end
+    computer_board.place(computer_cruiser, valid_cruiser_coordinates)
+    # computer_placement_message
   end
 
-  def player_ship_placement
-    # render computer board(false)
-    #render user board(false)
+  def computer_placement_message
+    puts computer_board.render
+    puts "I have laid out my ships on the grid."
+    puts "You now need to lay out your two ships."
+    puts "The Cruiser is three units long and the Submarine is two units long."
+    # player_cruiser_placement
+  end
 
-    puts "Place your CRUISER on the board. Example: A1, A2, A3" 
-    player_cruiser_placement = gets.chomp.upcase 
-    # coordinates must be valid- must be 3 coordinates or invalid
-    # if not valid "this is not a valid coordinate"
-    # -------- code to be written ---------
-
-    puts "Place your SUBMARINE on the board. Example: B1, C1"
-    player_sub_placement = gets.chomp.upcase
-    # coordinates must be valid- must be 2 coordinates or invalid 
-    # if not valid "this is not a valid coordinate"
-    # -------- code to be written ---------
-    show_board
+  def player_cruiser_placement
+    puts "Enter squares for your Cruiser (3 spaces):"
+    player_cruiser_input = gets.chomp.upcase.split 
+    if player_board.valid_placement?(player_cruiser, player_cruiser_input) == true 
+      player_board.place(player_cruiser, player_cruiser_input)
+    else
+      puts "Those are invalid coordinates. Please try again."
+      player_cruiser_placement 
+    end
+    # player_sub_placement
   end
 
   def show_board
